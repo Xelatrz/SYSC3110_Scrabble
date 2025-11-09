564296332c70cddffd60757d0ec93f5a85f812d6
Cole Galway <cole.galway@yahoo.com>
2025-11-09 18:58:49 -0500

    finished the gui frame so that every needed label/button exists.

---
+++ src/GameModelTest.java
@@ -0,0 +1,25 @@
+/**
+ * Test for the Game class
+ *
+ * @author Cole Galway
+ * @version 10/27/2025
+ */
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameModelTest {
+
+    @Test
+    @DisplayName("Testing adding a new player")
+    void addPlayer() {
+        GameModel game = new GameModel();
+        Player p1 = new Player("p1");
+        Player p2 = new Player("p2");
+
+        game.addPlayer(p1);
+        game.addPlayer(p2);
+
+        org.junit.jupiter.api.Assertions.assertEquals(game.players.size(), 2);
+    }
+}
