2295f71940073f63d26ff994dff62d72f282e0a6
Taylor Brumwell <taylor.brumwell@gmail.com>
2025-11-22 20:47:12 -0500

    feat: added crossword check and accepted word check. IMPORTANT NOTE: in the past we didn't use a boolean horizontal variable (just checked manually) so we never realized, but since our board is setup to be grid[row][col], they're actually flipped, meaning row is vertical. This confused me a lot but now that i've realized, it makes more sense. I wanted to document this in case anyone else gets stuck on this. ANOTHER IMPORTANT NOTE: a lot of the AIPlayer code could prove useful in validating Player plays, we may eventually want to migrate some of the functions to the Player function, or at least parts of them, and have AIPlayer access them since the AIPlayer class extends Player.

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -5,6 +5,7 @@
  * @version 11/20/2025
  */
 
+import java.io.StringBufferInputStream;
 import java.lang.reflect.Array;
 import java.util.*;
 
@@ -38,6 +39,7 @@ public class AIPlayer extends Player {
          return bestMove;
      }
 
+     //try all words connected to anchors on individual rows/columns of the board.
      private Move tryLine(Board board, int lineIndex, boolean horizontal) {
          //might not want the following functions in board, but rather in one of the Game files.
          String[] pattern = board.extractPattern(lineIndex, horizontal);
@@ -53,6 +55,7 @@ public class AIPlayer extends Player {
          return bestMove;
      }
 
+     //build valid words in the given area.
      private Move tryBuildWords(Board board, String[] pattern, int anchor, int lineIndex, boolean horizontal) {
          Move bestMove = null;
          for (int i = 0; i < GameModel.acceptedWords.size(); i++) {
@@ -64,9 +67,14 @@ public class AIPlayer extends Player {
              if (!isAnchored(word, anchor)) {
                  continue;
              }
-             if (!validateCrossWords(placedTiles, horizontal)) {
+             String primaryWord = buildPrimaryWord(placedTiles, board, horizontal);
+             if (!GameModel.acceptedWords.checkWord(primaryWord)) {
                  continue;
              }
+             if (!validateCrossWords(placedTiles, board, horizontal)) {
+                 continue;
+             }
+
              int score = board.simulateScore(placedTiles);
              if (bestMove == null || score > bestMove.score) {
                  bestMove = new Move(placedTiles, score);
@@ -75,17 +83,7 @@ public class AIPlayer extends Player {
          return bestMove;
      }
 
-     //check if connected
-     private boolean isAnchored(String word, int anchor) {
-        for (int start = 0; start <= Board.SIZE - word.length(); start++) {
-            if (anchor >= start && anchor < start + word.length()) {
-                return true;
-            }
-        }
-        return false;
-     }
-
-     //complete validity check
+     //major word validity check.
      private ArrayList<PlacedTile> fitWordToPattern(String word, String[] pattern, int lineIndex, boolean horizontal) {
         for (int start = 0; start <= Board.SIZE - word.length(); start++) {
             boolean conflict = false;
@@ -104,9 +102,9 @@ public class AIPlayer extends Player {
                     int col;
                     if (horizontal) {
                         row = lineIndex;
-                        col = position; //CHECK, this might be position + 1 depending on board grid
+                        col = position;
                     } else {
-                        row = position; //CHECK, this might be position + 1 depending on board grid
+                        row = position;
                         col = lineIndex;
                     }
                     tempPlacedTiles.add(new PlacedTile(row, col, new Tile(wordChar)));
@@ -119,7 +117,120 @@ public class AIPlayer extends Player {
         return null;
      }
 
-     private boolean validateCrossWords(ArrayList<PlacedTile> placedTiles, boolean horizontal) {
+    //check if word is connected to an anchor.
+    private boolean isAnchored(String word, int anchor) {
+        for (int start = 0; start <= Board.SIZE - word.length(); start++) { // do this based on pattern fit start point
+            if (anchor >= start && anchor < start + word.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    //build primary word as a string.
+    private String buildPrimaryWord(ArrayList<PlacedTile> placedTiles, Board board, boolean horizontal) {
+        if (placedTiles.isEmpty()) {
+            return "";
+        }
+
+        int row = placedTiles.getFirst().row;
+        int col = placedTiles.getFirst().col;
+        StringBuilder sb = new StringBuilder();
+
+        //REVISE THIS: a lot of code here duplicates Player code. once i'm done logic, see if i can fix this duplication
+        if (horizontal) {
+            int startCol = col;
+            int endCol =  col;
+            for (PlacedTile pt: placedTiles) {
+                if (pt.col < startCol) {
+                    startCol = pt.col;
+                }
+                if (pt.col > endCol) {
+                    endCol = pt.col;
+                }
+            }
+            while (startCol > 0 && board.getTile(row, startCol - 1) != null) {
+                startCol--;
+            }
+            while (endCol < Board.SIZE - 1 && board.getTile(row, endCol + 1) != null) {
+                endCol++;
+            }
+            for (int c = startCol; c <= endCol; c++) {
+                Tile tile = board.getTile(row, c);
+                if (tile != null) {
+                    sb.append(tile.getLetter());
+                } else {
+                    sb.append("_");
+                }
+            }
+        } else {
+            int startRow = row;
+            int endRow =  row;
+            for (PlacedTile pt: placedTiles) {
+                if (pt.row < startRow) {
+                    startRow =  pt.row;
+                }
+                if (pt.row > endRow) {
+                    endRow = pt.row;
+                }
+            }
+            while (startRow > 0 && board.getTile(startRow - 1, col) != null) {
+                startRow--;
+            }
+            while (endRow < Board.SIZE - 1 && board.getTile(endRow + 1, col) != null) {
+                endRow++;
+            }
+            for (int r = startRow; r <= endRow; r++) {
+                Tile tile = board.getTile(r, col);
+                if (tile != null) {
+                    sb.append(tile.getLetter());
+                } else {
+                    sb.append("_");
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    private boolean validateCrossWords(ArrayList<PlacedTile> placedTiles, Board board, boolean horizontal) {
+        for (PlacedTile pt : placedTiles) {
+            StringBuilder sb =  new StringBuilder();
+            int row = pt.row;
+            int col = pt.col;
+
+            if (horizontal) {
+                int startRow = row;
+                int endRow =  row;
+                while (startRow > 0 && board.getTile(startRow - 1, col) != null) {
+                    startRow--;
+                }
+                while (endRow < Board.SIZE - 1 && board.getTile(endRow + 1, col) != null) {
+                    endRow++;
+                }
+                for (int r  = startRow; r <= endRow; r++) {
+                    sb.append(board.getTile(r, col).getLetter());
+                }
+            } else {
+                int startCol = col;
+                int endCol =  col;
+                while (startCol > 0 && board.getTile(row, startCol - 1) != null) {
+                    startCol--;
+                }
+                while (endCol < Board.SIZE && board.getTile(row, endCol) != null) {
+                    endCol++;
+                }
+                for (int c = startCol; c <= endCol; c++) {
+                    sb.append(board.getTile(row, c).getLetter());
+                }
+            }
+            String crossWord = sb.toString();
+            if (crossWord.length() <= 1) {
+                continue;
+            }
+            if (!GameModel.acceptedWords.checkWord(crossWord.toLowerCase())) {
+                return false;
+            }
+        }
+        return true;
      }
 }
